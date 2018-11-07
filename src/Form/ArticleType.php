aa5f8b93e8e9727eab5b23fd2e44271a26262e9c
moroztaras <moroztaras@i.ua>
2018-11-07 19:40:23 +0200

    changle article field

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,7 +16,7 @@ class ArticleType extends AbstractType
         $builder->add('title', TextType::class, [
           'label' => 'Title'
         ]);
-        $builder->add('text', TextType::class, [
+        $builder->add('text', TextareaType::class, [
           'label' => 'Text'
         ]);
         $builder->add('author', TextType::class, [

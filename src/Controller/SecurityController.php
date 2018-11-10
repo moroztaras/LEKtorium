b74392e852abf52549b4021c595cc6cd205df9fa
moroztaras <moroztaras@i.ua>
2018-11-10 21:58:51 +0200

    Registration user

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,15 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/login", name="app_login")
+     * @Route("/login",  methods={"GET", "POST"}, name="app_login")
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
@@ -20,5 +24,28 @@ class SecurityController extends AbstractController
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/registration", methods={"GET", "POST"}, name="app_registration")
+     */
+    public function registrationAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword($passwordEncoder->encodePassword($user,$user->getPassword()));
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('article_list');
+        }
+
+        return $this->render('security/registration.html.twig', [
+          'user_registration' => $form->createView(),
+        ]);
     }
 }

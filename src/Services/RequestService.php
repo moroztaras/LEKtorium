a02da75d70989f4834c911814e338ba4f32ff62e
moroztaras <moroztaras@i.ua>
2018-11-19 15:29:17 +0200

    Add listener, add subscriber

---
+++ src/Services/RequestService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Services;
+
+use Symfony\Component\HttpFoundation\Request;
+
+class RequestService
+{
+    public function processRequest(Request $request)
+    {
+        $request = $request;
+    }
+}

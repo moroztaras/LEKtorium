1eafe5e3d5b73acdfb9c1fd9a78bb6f585a5df1f
moroztaras <moroztaras@i.ua>
2018-11-20 19:28:42 +0200

    New tests fot securiyy login

--- tests/SecurityControllerTest.php
+++ tests/SecurityControllerTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class SecurityControllerTest extends WebTestCase
@@ -10,29 +11,50 @@ class SecurityControllerTest extends WebTestCase
     public function testLoginAction()
     {
         $client = static::createClient();
-        $client->request('GET', '/login');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $client->request(Request::METHOD_GET, '/login');
+        $this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
     }
 
     public function testLogin()
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/login');
+        $crawler = $client->request(Request::METHOD_GET, '/login');
         $this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
-        $text = $crawler->filter('body > form > h1')->text();
+        $text = $crawler->filter('body > div > h1')->text();
         $this->assertEquals('Please sign in', $text);
     }
 
     public function testLoginFields()
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/login');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $crawler = $client->request(Request::METHOD_GET, '/login');
+        $this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
         $this->assertGreaterThan(
           0,
           $crawler->filter('html:contains("Email")')->count());
         $this->assertGreaterThan(
           0,
           $crawler->filter('html:contains("Password")')->count());
+    }
+
+    public function testLoginForm()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request(Request::METHOD_GET, '/login');
+
+        $this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
+
+        $text = $crawler->filter('body > div > h1')->text();
+        $this->assertEquals('Please sign in', $text);
+
+        $this->assertCount(3, $crawler->filter('input'));
+
+        $form = $crawler->filter('button[type=submit]')->form();
+        $form['email'] = 'moroztaras@i.ua';
+        $form['password'] = 'moroztaras';
+        $crawler = $client->submit($form);
+
+        $this->assertEquals(Response::HTTP_FOUND, $client->getResponse()->getStatusCode());
     }
 }

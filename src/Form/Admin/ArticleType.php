e9689253baf3923f0410358b9baa498a2a9e806c
moroztaras <moroztaras@i.ua>
2018-12-26 11:50:49 +0200

    new field - Upload Image, for downloading a image. Model for the form of a new article

--- src/Form/Admin/ArticleType.php
+++ src/Form/Admin/ArticleType.php
@@ -1,9 +1,11 @@
 <?php
 
-namespace App\Form;
+namespace App\Form\Admin;
 
 use App\Entity\Article;
+use App\Form\Admin\Model\ArticleModel;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -19,16 +21,27 @@ class ArticleType extends AbstractType
         $builder->add('text', TextareaType::class, [
           'label' => 'Text',
         ]);
+        $builder->add('image', FileType::class, [
+          'label' => 'Upload Image'
+        ]);
         $builder->add('tagsInput', TextType::class, [
           'label' => 'Tags',
         ]);
     }
 
+//    public function configureOptions(OptionsResolver $resolver)
+//    {
+//        $resolver->setDefaults([
+//          'data_class' => Article::class,
+//          'attr' => ['novalidate' => 'novalidate'],
+//        ]);
+//    }
     public function configureOptions(OptionsResolver $resolver)
     {
+        $resolver->setRequired('entity_manager');
         $resolver->setDefaults([
-          'data_class' => Article::class,
-          'attr' => ['novalidate' => 'novalidate'],
+          'data_class' => ArticleModel::class,
+          'entity_manager' => null,
         ]);
     }
 }

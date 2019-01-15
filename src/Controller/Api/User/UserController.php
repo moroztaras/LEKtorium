ca8752d10f4dfae10c2171edcff1b43bef3f1b31
moroztaras <moroztaras@i.ua>
2019-01-15 18:37:05 +0200

    API login user

--- src/Controller/Api/User/UserController.php
+++ src/Controller/Api/User/UserController.php
@@ -6,10 +6,12 @@ use App\Entity\User;
 use App\Exception\JsonHttpException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\SerializerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
  * Class UserController.
@@ -23,9 +25,15 @@ class UserController extends Controller
      */
     private $serializer;
 
-    public function __construct(SerializerInterface $serializer)
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $passwordEncoder;
+
+    public function __construct(SerializerInterface $serializer, UserPasswordEncoderInterface $passwordEncoder)
     {
         $this->serializer = $serializer;
+        $this->passwordEncoder = $passwordEncoder;
     }
 
     /**
@@ -35,7 +43,7 @@ class UserController extends Controller
     public function registrationAction(Request $request)
     {
         if (!$content = $request->getContent()) {
-            throw new JsonHttpException(400, 'Bad Request');
+            throw new JsonHttpException(Response::HTTP_BAD_REQUEST, 'Bad Request');
         }
         /** @var User $user */
         $user = $this->serializer->deserialize($request->getContent(), User::class, JsonEncoder::FORMAT);
@@ -45,4 +53,24 @@ class UserController extends Controller
 
         return $this->json($user);
     }
+
+       /**
+        * @Route("/login", name="api_login", methods={"POST"})
+        */
+       public function loginAction(Request $request)
+    {
+        if (!$content = $request->getContent()) {
+            throw new JsonHttpException(Response::HTTP_BAD_REQUEST, 'Bad Request');
+        }
+        $data = json_decode($content, true);
+        $user = $this->getDoctrine()->getRepository(User::class)->findOneBy(['email'=>$data['email']]);
+
+        if($user instanceof User) {
+            if($this->passwordEncoder->isPasswordValid($user,$data['password'])){
+                return ($this->json(['user'=>$user]));
+            }}
+
+        throw new JsonHttpException(Response::HTTP_BAD_REQUEST, 'Bad Request');
+    }
+
 }
